af560a9a33818b515526d671271d8f86f77517ff
Rafa <rafa@Rafas-iMac-Pro.local>
2025-07-23 16:12:01 -0300

    Adicionando a senha na tela

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Seleção de Elementos
 const generatePasswordButton = document.querySelector("#generate-password");
-const generatePasswordElement = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
 
 //Funções
 
@@ -20,15 +20,30 @@ const getSymbol = () => {
     return symbols[Math.floor(Math.random() * symbols.length)];
 };
 
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+    let password = "";
+    const passwordLength = 10;
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    ]
+    for(i = 0; i < passwordLength; i = i + 4) {
+        generators.forEach(() => {
+            const randomValue = 
+            generators[Math.floor(Math.random() * generators.length)]();
+        password += randomValue;
+        });
+    }
+    password = password.slice(0, passwordLength);
 
-
-
-console.log(getSymbol());
-
-
+    generatedPasswordElement.style.display = "block";
+    generatedPasswordElement.querySelector('h4').innerText =  password;
+};
 
 
 //Eventos
 generatePasswordButton.addEventListener("click", () => {
-    console.log('Teste');
+    generatePassword(getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol);
 });
